416200e3be9279c445fad5c94a0dd99271c56130
Amine Guenaoui <44202091+Amine-Guenaoui@users.noreply.github.com>
2021-12-05 15:13:43 +0100

    Add files via upload

---
+++ emails_extractor.py
@@ -0,0 +1,72 @@
+import cv2
+import numpy as np 
+from PIL import Image 
+# from matplotlib import pyplot as plt
+from pdfminer3.layout import LAParams #LTTextBox
+from pdfminer3.pdfpage import PDFPage
+from pdfminer3.pdfinterp import PDFResourceManager
+from pdfminer3.pdfinterp import PDFPageInterpreter
+# from pdfminer3.converter import PDFPageAggregator
+from pdfminer3.converter import TextConverter
+from pathlib import Path
+import io
+# must be a loop . but for now one pic is enough 
+# nzid pdf , w characters 
+#for verifying if file is an email 
+import re
+ 
+# Make a regular expression
+# for validating an Email
+regex = r'\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,}\b'
+
+word_number = 0
+old = 0
+def check(email):
+
+    if(re.fullmatch(regex, email)):
+        # print("Valid Email")
+        return True
+    else:
+        # print("Invalid Email")
+        return False
+# os.system('dir c:\\')
+resource_manager = PDFResourceManager()
+fake_file_handle = io.StringIO()
+converter = TextConverter(resource_manager, fake_file_handle, laparams=LAParams())
+page_interpreter = PDFPageInterpreter(resource_manager, converter)
+with open('liste_emails.txt', 'w') as f:
+    with open('orig.pdf', 'rb') as fh:
+        Pages = PDFPage.get_pages(fh,caching=True,check_extractable=True)
+        # incrementing = 0
+        for pageNumber,page in enumerate(Pages):
+
+            # must exclude last
+            print("progressing .... " + str(pageNumber+1))
+            text = ""
+            page_interpreter.process_page(page)
+            text = fake_file_handle.getvalue()
+            word_content = text.split().copy()
+            new = len(word_content)
+            # print("new : "+str(new))
+            # print(" old : "+str(old))
+            # print("current: "+str(new - old))
+            # print(word_content)
+            if pageNumber > 0 :
+                del word_content[0:old]
+            old += len(word_content)
+            print(len(word_content))
+            for word in word_content:
+                if check(word):
+                    if "yhoo" in word:
+                        # print("correction de l;email ")
+                        word = word.replace("yhoo", "yahoo")
+                    f.write(word)
+                    
+                    f.write("\n")
+                    
+                    print(word)
+        # close open handles
+    converter.close()
+    fake_file_handle.close()
+    fh.close()
+f.close()
